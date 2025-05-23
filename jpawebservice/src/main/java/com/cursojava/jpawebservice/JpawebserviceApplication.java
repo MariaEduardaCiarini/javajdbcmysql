b102e73fd0d1c008411f7b1348d8e34802edf8c4
Duda <mariaeduardaciarini03@gmail.com>
2025-05-23 17:42:45 -0300

    Projeto Web Services Springboot sts JPA / Hibernate (aula criação projeto)

---
+++ jpawebservice/src/main/java/com/cursojava/jpawebservice/JpawebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.cursojava.jpawebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpawebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpawebserviceApplication.class, args);
+	}
+
+}
