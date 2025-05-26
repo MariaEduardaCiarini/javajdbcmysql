aeb62b209bdbf567787d0c4c7075a9e6ae1ea0db
Duda <mariaeduardaciarini03@gmail.com>
2025-05-26 03:16:12 -0300

    Entidade Product

---
+++ jpawebservice/src/main/java/com/cursojava/jpawebservice/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.cursojava.jpawebservice.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cursojava.jpawebservice.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
