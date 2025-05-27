16a3eb69eee94d7275f763738ccf8ae4974bcb56
Duda <mariaeduardaciarini03@gmail.com>
2025-05-26 21:57:30 -0300

    Tratamento de exceção - findById
    {
        "timestamp": "2025-05-27T00:54:41Z",
        "status": 404,
        "error": "Resource not found",
        "message": "Resource not found. Id 7",
        "path": "/users/7"
    }

--- jpawebservice/src/main/java/com/cursojava/jpawebservice/services/UserService.java
+++ jpawebservice/src/main/java/com/cursojava/jpawebservice/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.cursojava.jpawebservice.entities.User;
 import com.cursojava.jpawebservice.repositories.UserRepository;
+import com.cursojava.jpawebservice.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 
 	public User insert(User obj) {
